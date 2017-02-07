39f593980e6c7d8d0b834f3fe8f9439729ef025c
Leon <assyr@leond.co.uk>
2017-02-07 20:54:18 +0000

    API Controller - Using IActionResult instead of Json for our HttpGet API request.

---
+++ src/TheWorld/Controllers/Web/Api/TripsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheWorld.Models;
+
+namespace TheWorld.Controllers.Web.Api
+{
+    public class TripsController : Controller
+    {
+        //Specify the route below to call our JsonResult which returns a trip with the name "My Trip"
+        [HttpGet("api/trips")]
+        public IActionResult Get()
+        {
+            /*if (true)
+                return BadRequest("Bad things happened");*/ //Handling a bad request
+
+            return Ok(new Trip() { Name = "My Trip" });
+        }
+    }
+}
