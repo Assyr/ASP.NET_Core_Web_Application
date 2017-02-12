49cc63f09792b127217025efb69e6f87bfb0e125
Leon <assyr@leond.co.uk>
2017-02-12 15:33:19 +0000

    Initial geographic coordinate service implementation - This will be able to take a location, resolve it and return the longitude and latitude of this particular location.

---
+++ src/TheWorld/Services/GeoCoordsService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWorld.Services
+{
+    public class GeoCoordsService
+    {
+        private ILogger<GeoCoordsService> _logger;
+
+        public GeoCoordsService(ILogger<GeoCoordsService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<GeoCoordsResult> GetCoordAsync(string name)
+        {
+            //Initialize an instance of GeoCoordsResult
+            var result = new GeoCoordsResult()
+            {
+                Success = false,
+                Message = "Failed to get coordinates"
+            };
+
+            var apiKey = "";
+            var encodedName = WebUtility.UrlEncode(name);
+            //var url = $
+        }
+    }
+}
