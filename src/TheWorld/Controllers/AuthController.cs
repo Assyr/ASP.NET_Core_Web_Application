7137fca2ca2498c99928b8d9e87e0b559c760ea5
Leon <assyr@leond.co.uk>
2017-02-16 00:29:26 +0000

    Controller for Authentication called 'Auth' that has a 'Login()' IActionResult implementation which checks to see if the user is authenticated and if they are, they're redirected to the trips view in the app controller. As per usual, a ViewModel is made for login that creates the properties we will be storing our data to and validating from the Login.cshtml. The login.cshtml is a simple login with a username/password field and a submit button. These controls are told which validation to use from the model injected at the top (LoginViewModel) using asp-validation-for.

---
+++ src/TheWorld/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        public IActionResult Login()
+        {
+            //If the user is already authenticated, no need to log them in again
+            if(User.Identity.IsAuthenticated)
+            {
+                //Redirect them to the 'Trips' action in our 'App' controller
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+
+    }
+}
