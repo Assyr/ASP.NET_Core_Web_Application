526b6357647c41179f030f4014ddd66e00ad7f1d
Leon <assyr@leond.co.uk>
2017-02-22 21:18:09 +0000

    Using our first AngularJS controller

---
+++ src/TheWorld/wwwroot/js/tripsController.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    //Getting the existing module
+    angular.module("app-trips")
+    .controller("tripsController", tripsController);
+
+    function tripsController() {
+
+        var vm = this;
+
+        vm.name = "Leon";
+    }
+
+})();
