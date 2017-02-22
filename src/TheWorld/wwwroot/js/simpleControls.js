194cee876c2e3239cda7b94ed243b6642f895a56
Leon <assyr@leond.co.uk>
2017-02-22 22:53:47 +0000

    Creating and using a directive in AngularJS

---
+++ src/TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,18 @@
+﻿//simpleControls.js
+(function () { //Immediately invoked function
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor); //Create a directive called waitCursor that will use the 'waitCursor' function implementation / return
+
+    //waitCursor definition
+    function waitCursor() {
+        return {
+            scope: {
+                show: "=displayWhen"
+            },
+            restrict: "E",
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+})();
